4594fb136af389567e5d76fef2b146ab65e735d7
Amakris12 <71471809+Amakris12@users.noreply.github.com>
2021-03-23 17:26:33 -0700

    Add files via upload

---
+++ script/resume.js
@@ -0,0 +1,20 @@
+$("#first").click(function(){
+    console.log("show");
+    $("#Resume").fadeIn(2000);
+    $("#Service").fadeOut(2000);
+});
+$("#two").click(function(){
+    console.log("show");
+    $("#Job").fadeIn(2000);
+    $("#Education").fadeOut(2000);
+});
+$("#third").click(function(){
+    console.log("show");
+    $("#Resume").fadeOut(2000);
+    $("#Service").fadeIn(2000);
+});
+$("#four").click(function(){
+    console.log("show");
+    $("#Job").fadeOut(2000);
+    $("#Education").fadeIn(2000);
+});
---
+++ script/skills.js
@@ -0,0 +1,8 @@
+$("#First").click(function(){
+    console.log('show');
+    $('#skills').fadeIn(2000);
+});
+$("#Two").click(function(){
+    console.log('show');
+    $('#Training').fadeIn(2000);
+});
