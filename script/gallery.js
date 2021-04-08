914dac20a9ad3bfe8c80a2fe57e1359b2de2e489
Amakris12 <71471809+Amakris12@users.noreply.github.com>
2021-04-08 17:54:05 -0500

    Add files via upload

--- script/gallery.js
+++ script/gallery.js
@@ -4,6 +4,7 @@ $('#website').on('click', function(){
     $('#website').hide(100);
     $('.card').toggle(500);
     $('h3').show(1000);
+    $('#closer').show(1000);
     $("#background").css("background-image","url('../images/batman.jpg')",1000).css('margin-left','2rem')
 });
 
