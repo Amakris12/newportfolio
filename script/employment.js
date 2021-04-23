5f068a4281c4a33d6eddadf10e302b6fef302af4
Amakris12 <71471809+Amakris12@users.noreply.github.com>
2021-04-22 18:17:29 -0700

    Add files via upload

--- script/employment.js
+++ script/employment.js
@@ -1,9 +1,9 @@
 $("#job").click(function(){
-    $("#divjob").fadeIn(2000);
-    $("#hid").fadeIn(2000);
+    $("#employ").fadeIn(2000);
+    $("#top").fadeOut(2000);
 });
 $("#com").click(function(){
-    $("#divcom").fadeIn(2000);
-    $("#hid2").fadeIn(2000);
+    $("#service").fadeIn(2000);
+    $("#top").fadeOut(2000);
 });
 
